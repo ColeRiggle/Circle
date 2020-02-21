0c22dbf73ab350f20fdf8a3ba41519e7cafb39bc
Cole Riggle <coleriggle@gmail.com>
2020-02-20 19:56:42 -0500

    Added neccessary files

---
+++ Circle/Helpers/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Appstore
+//
+//  Created by Cole Riggle on 12/30/19.
+//  Copyright © 2019 Cole Riggle. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String, font: UIFont, numberOfLines: Int = 1) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+        self.numberOfLines = numberOfLines
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+        
+    }
+}
